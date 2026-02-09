0d8488a17619908eb84b72fa00be71f7650ced55
Praanshu-joshi <impj1729@gmail.com>
2026-02-10 02:24:18 +0530

    created test APIs

--- src/app.js
+++ src/app.js
@@ -3,9 +3,97 @@ require('dotenv').config();
 const dbConnect=require('./config/database')
 
 
+const User= require('./models/user')
+
+
 const app= express();
 
 
+//middlewares 
+
+//parse json data
+app.use(express.json())
+
+// parse form data 
+app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+
+
+
+//configure routes here->>>>
+
+
+// post signup
+app.post('/signup',async(req,res)=>{
+    
+    const data = req.body;
+
+    try{
+        await User.create(data);
+        res.send("user added successfully")
+    }
+    catch(e){
+        res.status(400).send("cannot add user")
+    }
+
+})
+
+
+// get user by Emailid 
+app.get('/user',async(req,res)=>{
+    const {emailId:userEmail}=req.body;
+    try{
+        const user=await User.findOne({emailId:userEmail});
+        if(!user)return res.send("no such user exist")
+        return res.send(user)
+    }
+    catch(e){
+        res.status(404).send("something went wrong")
+    }
+})
+
+// get /feed api -> get all users from database
+
+app.get('/feed',async(req,res)=>{
+    try{
+        const users= await User.find({});
+        res.send(users);
+    }
+    catch(e){
+        res.status(404).send("something went wrong")
+    }
+})
+
+
+// delete /user -> delete user by emailid 
+app.delete('/user',async(req,res)=>{
+    const {emailId:email}=req.body;
+    try{
+        await User.findOneAndDelete({emailId:email});
+       return  res.send("user deleted ")
+    }
+    catch(e){
+        res.status(404).send("something went wrong")
+    }
+})
+
+
+// patch /user -> update user 
+app.patch('/user',async(req,res)=>{
+
+    try{
+        await User.findOneAndUpdate({emailId:req.body.emailId},req.body)
+        res.send('user updated')
+    }
+    catch{
+        res.status(404).send("something went wrong")
+    }
+
+})
 
 
 
